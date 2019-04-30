7dd5223410a71c7224aa608436b1551f2964fdba
Ricardo de Carvalho <skapitao@gmail.com>
2019-04-30 13:11:28 -0300

    feat: Commit initial

---
+++ lib/ui/contact_page.dart
@@ -0,0 +1,109 @@
+import 'dart:io';
+
+import 'package:agenda_contatos/helpers/contact_helper.dart';
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatefulWidget {
+  final Contact contact;
+
+  ContactPage({this.contact});
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+
+  final _nameFocus = FocusNode();
+
+  bool _userEdited = false;
+  Contact _editContact;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.contact == null) {
+      _editContact = Contact();
+    } else {
+      _editContact = Contact.fromMap(widget.contact.toMap());
+
+      _nameController.text = _editContact.name;
+      _emailController.text = _editContact.email;
+      _phoneController.text = _editContact.phone;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(_editContact.name ?? "Novo Contato"),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (_editContact.name != null &&_editContact.name.isNotEmpty) {
+            Navigator.pop(context, _editContact);
+          } else {
+            FocusScope.of(context).requestFocus(_nameFocus);
+          }
+        },
+        child: Icon(Icons.save),
+        backgroundColor: Colors.red,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              child: Container(
+                width: 140,
+                height: 140,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: _editContact.img != null
+                            ? FileImage(File(_editContact.img))
+                            : AssetImage("images/person.png"))),
+              ),
+            ),
+            TextField(
+              controller: _nameController,
+              focusNode: _nameFocus,
+              decoration: InputDecoration(labelText: "Nome:"),
+              onChanged: (text) {
+                _userEdited = true;
+                setState(() {
+                  _editContact.name = text;
+                });
+              },
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: "E-mail:"),
+              onChanged: (text) {
+                _userEdited = true;
+                _editContact.email = text;
+              },
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: _phoneController,
+              decoration: InputDecoration(labelText: "Telefone:"),
+              onChanged: (text) {
+                _userEdited = true;
+                _editContact.phone = text;
+              },
+              keyboardType: TextInputType.phone,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
